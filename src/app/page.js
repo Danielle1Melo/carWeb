6d927cc189b1ae5e98d01a2edf23712f6c28ec8e
Danielle <danielleesilva.4@gmail.com>
2025-02-04 21:58:03 -0400

    start project

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,64 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import styled from "../style/login.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main className={styled.container}>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+      <section className={styled.esquerda}>
+        <div className={styled.esquerdaTitulo}>
+          <h1>Bem-Vindo</h1>
+          <p>Crie sua conta agora mesmo.</p>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </section>
+
+      <section className={styled.direita}>
+        <div className={styled.direitaTitulo}>
+          <Image />
+          <p>Web Cars</p>
+        </div>
+
+        <div className={styled.containerForm}>
+          <p>Criar sua conta</p>
+
+          <main className={styled.form}>
+            <div>
+              <label>Nome:</label>
+              <input type="text" placeholder="Nome" />
+            </div>
+
+            <div>
+              <label>CNPJ:</label>
+              <input type="number" placeholder="00000000000000" />
+            </div>
+
+            <div>
+              <label>Telefone:</label>
+              <input type="number" placeholder="99 99999-9999" />
+            </div>
+
+            <div>
+              <label>Localização:</label>
+              <input type="text" placeholder="ifro" />
+            </div>
+
+            <div>
+              <label>Senha:</label>
+              <input type="text" placeholder="****" />
+            </div>
+
+            <div>
+              <label>Confirmar senha:</label>
+              <input type="text" placeholder="****" />
+            </div>
+          </main>
+
+          <div>
+            <button type="submit" className={styled.btnCriarConta}>Criar Conta</button>
+            <p>Já possui uma conta? <a href="#">Login</a></p>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
