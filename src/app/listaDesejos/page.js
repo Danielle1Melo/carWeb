e7dd654541b5f6497c3665657c3b2e54566cf1c3
Danielle <danielleesilva.4@gmail.com>
2025-02-08 00:37:20 -0400

    creating car cards

--- src/app/listaDesejos/page.js
+++ src/app/listaDesejos/page.js
@@ -1,59 +1,22 @@
-import Image from "next/image";
+import CarList from "@/components/carList/CarList";
 import styled from "../../style/listaDesejos.module.css";
-import carImage from "../../assets/carImage.svg"
 import Header from "@/components/header/Header";
-
-const carItens = [
-    {
-        id: 1,
-        imagem: carImage,
-        desc: "volkswagen gol",
-    },
-    {
-        id: 2,
-        imagem: carImage,
-        desc: "volkswagen gol",
-    },
-    {
-        id: 3,
-        imagem: carImage,
-        desc: "volkswagen gol",
-    },
-    {
-        id: 4,
-        imagem: carImage,
-        desc: "volkswagen gol",
-    },
-    {
-        id: 5,
-        imagem: carImage,
-        desc: "volkswagen gol",
-    },
-    {
-        id: 6,
-        imagem: carImage,
-        desc: "volkswagen gol",
-    },
-    {
-        id: 7,
-        imagem: carImage,
-        desc: "volkswagen gol",
-    },
-]
 
 export default function ListaDesejos() {
     return (
         <div className={styled.container}>
             <Header />
-            {/* {carItens.map((item) => {
-                return (
-                    <div className={styled.cards} key={item.id}>
-                        <Image src={item.imagem} alt="" />
-                        <p>{item.desc}</p>
-                        <button className={styled.button}>Veja mais</button>
-                    </div>
-                )
-            })} */}
+
+            <button className={styled.btnVoltar}>Voltar</button>
+
+            <div className={styled.title}>
+                <h1>Lista de Desejos</h1>
+                <button className={styled.btnCompartilhar}>Compartilhar</button>
+            </div>
+
+            <CarList />
+
+
         </div>
     )
 }
